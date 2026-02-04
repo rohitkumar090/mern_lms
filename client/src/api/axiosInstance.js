1f83ffa1fa8cbf7f38b44f361eda926369c1d654
Rohit Kumar <rohitkumar90153@gmail.com>
2026-02-04 17:03:11 +0530

    URL changed

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://mern-lms-htxr.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
